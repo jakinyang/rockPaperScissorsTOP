0028f95c6454b4287c01e941e7fd3c9e975f36d4
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-05 12:20:22 -0700

    Removes submit button functionality
    Due to removing the submit button, event listener for userChoice buttons refactored to retrieve both user and computer choice and to run the game with retrieved choices and then output the result to winnerDisplay.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -3,28 +3,31 @@ let winnerOutput = '';
 
 /* Variable that will hold value retrieved from button click event */
 let userChoice = '';
+let computerChoice = '';
 
 /* Retrieving user choice from button DOM event */
 
 // Variables to hold DOM buttons
-const rBtn = document.querySelector('#rock');
-const pBtn = document.querySelector('#paper');
-const sBtn = document.querySelector('#scissors');
 const btns = document.querySelectorAll('.button');
 
 // Function to output eventListener result to userChoice
 
-const getUserChoice = (e) => {
+const runGame = (e) => {
   userChoice = e.target.textContent.toLowerCase();
+  computerChoice = getComputerChoice();
+  game(computerChoice, userChoice);
+  document.querySelector('#winnerTitle').textContent = winnerOutput;
+  console.log(userChoice);
+  console.log(computerChoice);
 }
 
 // Adding event listeners
 btns.forEach(btn => {
-  btn.addEventListener('click', getUserChoice)
+  btn.addEventListener('click', runGame)
 })
 
 /* Var that holds value from computer choice generator */
-let computerChoice = () => {
+let getComputerChoice = () => {
   let randomIndex = Math.ceil(Math.random() * 3);
   switch (randomIndex) {
     case 1:
@@ -46,35 +49,52 @@ let computerChoice = () => {
 const game = (compChoice, playerChoice) => {
   if (playerChoice === 'rock') {
     if (compChoice === 'paper') {
+      winnerOutput = 'Computer wins!';
       return 'Computer wins!';
     } else if (compChoice === 'scissors') {
+      winnerOutput = 'Player wins!';
       return 'Player wins!';
     } else {
+      winnerOutput = 'Tie!';
       return 'Tie!';
     } 
   } else if (playerChoice === 'paper') {
     if (compChoice === 'scissors') {
+      winnerOutput = 'Computer wins!';
       return 'Computer wins!';
     } else if (compChoice === 'rock') {
+      winnerOutput = 'Player wins!';
       return 'Player wins!';
     } else {
+      winnerOutput = 'Tie!';
       return 'Tie!';
     }
   } else if (playerChoice === 'scissors') {
     if (compChoice === 'rock') {
+      winnerOutput = 'Computer wins!';
       return 'Computer wins!';
     } else if (compChoice === 'paper') {
+      winnerOutput = 'Player wins!';
       return 'Player wins!';
     } else {
+      winnerOutput = 'Tie!';
       return 'Tie!';
     }
   }
+  console.log(winnerOutput);
+  document.querySelector('#winnerTitle').textContent = winnerOutput;
 }
 
 /* Playgame function */
 
-const playGame = (computerChoice, userChoice) => {
-  winnerOutput = game(computerChoice, userChoice);
-  console.log(winnerOutput);
-}
+/* const playGame = (computerChoice, userChoice) => {
+  game(computerChoice, userChoice);
+  console.log(computerChoice());
+  console.log(userChoice)
+} */
 
+/* Event listener to initiate playGame */
+
+/* const submitBtn = document.querySelector('.submitBtn');
+
+submitBtn.addEventListener('click', game(computerChoice, userChoice)); */

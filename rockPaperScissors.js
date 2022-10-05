5f3b29c3a76279107b0ee2cc6f7378383a170053
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-05 10:25:38 -0700

    Adds game mechanic to rockPaperScissors.js file
    Adding the game mechanic to determine winner output based on userChoice v. computerChoice.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,11 @@
+
+/* Variable that holds output for winner that will be displayed in .winnerDisplay */
 let winnerOutput = '';
 
+/* Variable that will hold value retrieved from button click event */
 let userChoice = '';
 
+/* Var that holds value from computer choice generator */
 let computerChoice = () => {
   let randomIndex = Math.ceil(Math.random() * 3);
   switch (randomIndex) {
@@ -17,5 +21,34 @@ let computerChoice = () => {
     default:
       return 'OMG WTF IS HAPPENING'
       break;
+  }
+}
+
+/* Game: takes in two choices and outputs the winner */
+const game = (compChoice, playerChoice) => {
+  if (playerChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'Computer wins!';
+    } else if (computerChoice === 'scissors') {
+      return 'Player wins!';
+    } else {
+      return 'Tie!';
+    } 
+  } else if (playerChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'Computer wins!';
+    } else if (computerChoice === 'rock') {
+      return 'Player wins!';
+    } else {
+      return 'Tie!';
+    }
+  } else if (playerChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'Computer wins!';
+    } else if (computerChoice === 'paper') {
+      return 'Player wins!';
+    } else {
+      return 'Tie!';
+    }
   }
 }
